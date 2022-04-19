62e01cbad1f759a6c0817428749b651423448516
MacBook_Pro <jmyoon555@gmail.com>
2022-04-19 14:46:29 +0900

    project 및 study 관련 story 제작 및 컴포넌트 제작

--- src/component/Box/ProjectBox.tsx
+++ src/component/Box/ProjectBox.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
+import Styled from "styled-components";
 
 interface Props {
+    /**
+     * 프로젝트 이름
+     */
     Title: string
+    /**
+     * 프로젝트 설명
+     */
+    Description: string
 }
 
-const ProjectBox: React.FunctionComponent<Props> = ({Title}) => {
+const ProjectBox: React.FunctionComponent<Props> = ({Title,Description}) => {
     return (
-        <div style={{backgroundColor: 'red', width: '70%', height: '150px'}}>
-            {Title}
-        </div>
+        <ProjectContainer>
+            <h1>{Title}</h1>
+            <ProjectDescription>
+                {Description}
+            </ProjectDescription>
+        </ProjectContainer>
     );
 };
+
+const ProjectContainer = Styled.div`
+    width: calc(100%-40px);
+    height: 130px;
+    background-color: white;
+    border-radius: 6px;
+    border: 1px solid #000;
+    display: flex;
+    flex-direction: column;
+    padding: 20px 0 0 40px;
+`
+
+const ProjectDescription = Styled.p`
+    margin: 0;
+    width: 95%;
+    overflow: hidden; 
+    text-overflow: ellipsis;
+    white-space: nowrap;
+`
 
 export default ProjectBox;
---
+++ src/component/Box/StudyBox.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import Styled from "styled-components";
+
+interface Props {
+    /**
+     *  공부하는 것에 대한 이미지 또는 아이콘
+     */
+    Img: string,
+    /**
+     *  제목
+     */
+    Title: string,
+    /**
+     * 공부하는 것에 대한 간략한 내용
+     */
+    Description: string,
+    /**
+     * 공부와 관련된 태그
+     */
+    Tag?: string[]
+}
+
+
+const StudyBox:React.FunctionComponent<Props> = ({Img, Title, Description, Tag}) => {
+    return (
+        <StudyContainer>
+            <img src={Img} alt={''} style={{width: '100%', height: '70%',borderRadius: '6px 6px 0 0'}}/>
+            <StudyContent>
+                <h4>{Title}</h4>
+                <p>{Description}</p>
+                <div style={{display: 'flex',marginLeft: '18px'}}>
+                {Tag?.map((v)=>
+                    <div style={{margin: '2px', borderRadius:'12px', border: '1px solid blue',padding: "2.5px"}}>
+                        {v}
+                    </div>
+                )}
+                </div>
+            </StudyContent>
+        </StudyContainer>
+    );
+};
+
+const StudyContainer = Styled.div`
+    width: 340px;
+    height: 350px;
+    border-radius: 6px;
+    background-color: white;
+    border: 1px solid #000;
+    background-color: gray;
+`
+
+const StudyContent = Styled.div`
+    h4{
+        margin: 5px 20px 0 20px;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+    }
+    p:first-of-type{
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2; 
+        -webkit-box-orient: vertical;
+        margin: 5px 20px 0 20px;
+    }
+    
+`
+
+export default StudyBox;
